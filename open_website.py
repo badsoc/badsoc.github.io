1157f15e8ea6bf67b6c7ae87413c18bb820fcd1c
badsoc <98851284+badsoc@users.noreply.github.com>
2023-09-28 22:10:17 +0100

    Update open_website.py
    test of having redirect to LUSU page

--- open_website.py
+++ open_website.py
@@ -1,8 +1,11 @@
 import webbrowser
 def run():
-    pathToIndexHtmlFile = ''
+    '''pathToIndexHtmlFile = ''
     websiteTestingAddress = 'file:///' + pathToIndexHtmlFile
-    webbrowser.open(websiteTestingAddress, new=2)
+    webbrowser.open(websiteTestingAddress, new=2)'''
+    self.send_response(301)
+    self.send_header('Location','https://lancastersu.co.uk/groups/badminton-society')
+    self.end_headers()
 
 if __name__ == '__main__':
     run()
